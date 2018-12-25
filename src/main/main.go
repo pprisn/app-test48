c7b88508edbea454c7e4ca8f9851e3427234559a
Popurey Sergey <pprisn@yandex.ru>
2018-12-26 00:11:43 +0300

    Weekday

--- src/main/main.go
+++ src/main/main.go
@@ -5,6 +5,7 @@ import (
 	"regexp"
 
 	//	"bytes"
+	"time"
 	"encoding/json"
 	"io/ioutil"
 	"log"
@@ -21,6 +22,7 @@ import (
 // установка зависимостей gb vendor fecth gopkg.in/telegram-bot-api.v4
 // установка зависимостей из манифеста gb vendor restore
 //структура данных ответа от api.icndb.com
+// { "type": "success", "value": { "id": 563, "joke": "Chuck Norris causes the Windows Blue Screen of Death.", "categories": ["nerdy"] } }
 type Joke struct {
 	ID   uint32 `json: "id"`
 	Joke string `json: "joke"`
@@ -31,6 +33,7 @@ type JokeResponse struct {
 }
 
 //Структура данных ответа от https://translate.yandex.net/api/v1.5/tr.json/translate
+// Пример на запрос Hellow Jack, ответ: {"code":200,"lang":"en-ru","text":["\"Хеллоу Джек\""]}
 type TranslateJoke struct {
 	CODE uint32   `json: "code"`
 	Lang string   `json: "lang"`
@@ -43,13 +46,25 @@ var buttons = []tgbotapi.KeyboardButton{
 	tgbotapi.KeyboardButton{Text: "Прикол на русском"},
 }
 
+var Wd = map[string]string{
+		"Sunday": "Воскресенье",
+		"Monday": "Понедельник",
+		"Tuesday": "Вторник",
+		"Wednesday": "Среда",
+		"Thursday": "Четверг",
+		"Friday": "Пятница",
+		"Saturday":"Суббота",
+	}
+
+
+
 var Keytg string
 var Keyyandex string
 
 func init() {
 	Keytg = os.Getenv("KEYTG")
 	Keyyandex = os.Getenv("KEYYANDEX")
-}
+ }
 
 //При старте приложения, оно скажет телеграму ходить с обновлениями по этому URL
 
@@ -121,6 +136,8 @@ func main() {
 
 	//Регулярное выражение для запроса данных по объекту индекс ОПС
 	var validCASE = regexp.MustCompile(`(?m)(^ops[0-9]{6})|(^OPS[0-9]{6})|(^Ops[0-9]{6})|(^Опс[0-9]{6})|(^ОПС[0-9]{6})$`)
+	//var keywd string
+        var sWd string
 
 	// Читаем данные из канала updates и выполняем соответсвующие им действия
 	for update := range updates {
@@ -136,7 +153,10 @@ func main() {
 		default:
 			if validCASE.MatchString(update.Message.Text) {
 				//Если пользователь выполнил запрос opsINDEX
-				message = tgbotapi.NewMessage(update.Message.Chat.ID, "Вы запросили данные о почтовом отделении "+update.Message.Text)
+    				
+    				//keywd =fmt.Sprintf("%s", time.Now().Weekday())
+                                sWd = fmt.Sprintf(Wd[fmt.Sprintf("%s", time.Now().Weekday())])
+				message = tgbotapi.NewMessage(update.Message.Chat.ID, "Сегодня: "+ sWd+" Вы запросили данные о почтовом отделении "+update.Message.Text)
 				log.Printf("Запрос данных %s", update.Message.Text)
 			} else {
 				message = tgbotapi.NewMessage(update.Message.Chat.ID, `Press "Get Joke" to receive joke`)
