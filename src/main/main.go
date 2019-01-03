1ed4bceb844b34dcd09dc64052739832c905f070
Popurey Sergey <pprisn@yandex.ru>
2019-01-03 10:38:27 +0300

    +Translate text

--- src/main/main.go
+++ src/main/main.go
@@ -140,8 +140,7 @@ func main() {
 	var validCASE = regexp.MustCompile(`(?m)(^ops[0-9]{6})|(^OPS[0-9]{6})|(^Ops[0-9]{6})|(^Опс[0-9]{6})|(^ОПС[0-9]{6})$`)
 	//Регулярное выражение для запроса данных трек номера Регион курьер Липецк 15 или 17 символов 000020004000085
 	var validRKLIP = regexp.MustCompile(`(?m)^(([0-9]{15})|([0-9]{17}))$`)
-	//regexp.MustCompile(`(?m)(?m)^(([0-9]{15})|([0-9]{17}))$`)
-	var validTranslate = regexp.MustCompile(`(?m)(?m)(^[a-z-A-Z].*$)`)
+	var validTranslate = regexp.MustCompile(`(?m)(^[a-z-A-Z].*$)`)
 	//var keywd string
 	var sWd string
 	// Читаем данные из канала updates и выполняем соответсвующие им действия
@@ -167,7 +166,7 @@ func main() {
 				message = tgbotapi.NewMessage(update.Message.Chat.ID, req2rkLip(string(update.Message.Text)))
 			} else if validTranslate.MatchString(update.Message.Text) == true {
 				// Поступил запрос текста на английском - переведем его.
-				message = tgbotapi.NewMessage(update.Message.Chat.ID, req2rkLip(string(update.Message.Text)))
+				message = tgbotapi.NewMessage(update.Message.Chat.ID, getTranslate(update.Message.Text))
 
 			} else {
 				message = tgbotapi.NewMessage(update.Message.Chat.ID, `Уточните Штриховой Почтовый Идентификатор, пожалуйста. И повторите запрос.`)
