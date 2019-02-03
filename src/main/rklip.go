055a2f3f148d269470459da5d5f622971324557a
Popurey Sergey <pprisn@yandex.ru>
2019-02-03 15:08:02 +0300

    work

--- src/main/rklip.go
+++ src/main/rklip.go
@@ -94,8 +94,10 @@ func req2rkLip(barcode string) string {
 		return sDelivstatus
 		//log.Fatal(err)
 	}
-
 	defer resp.Body.Close()
+	//удалим все \n\t\r из полученных данных
+	htmlData = []byte(strings.TrimSpace(string(htmlData)))
+
 
 	trk := RKResp{}
 	// Если содержимое htmlDtat не будет соответствовать структуре RKResp будет panic
@@ -184,14 +186,15 @@ func req2rkLipAttach(attachment string) string {
 		return sDelivstatus
 		//log.Fatal(err)
 	}
-
+	//удалим все \n\t\r из полученных данных
+	htmlData = []byte(strings.TrimSpace(string(htmlData)))
 	defer resp.Body.Close()
 
 	trk := RKResp{}
 	// Если содержимое htmlDtat не будет соответствовать структуре RKResp будет panic
 	// выполним проверку на соответствие htmlData структе RKResp
 	// Проверка на валидность структуры htmlData, если не валидна - заполняем пустыми данными
-	log.Printf("htmlData= %v\n",string(htmlData))
+	log.Printf("htmlData= %v\n",strings.TrimSpace(string(htmlData)))
 	var validRKLip = regexp.MustCompile(`(?)(^\[\{"barcode":.*"attachment":.*"whom":.*"address":.*"postoffice":.*"delivery_site":.*"receipt_date":.*"delivery_status":.*"delivery_status_name":.*"delivery_date":.*\}\])$`)
 	if !validRKLip.MatchString(strings.TrimSpace(string(htmlData))) {
 		htmlData = RKResp2nilbyte()
